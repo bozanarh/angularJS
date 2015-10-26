3a23a3a4e1d8fbf4efc82c637ae827a65869ed5b
Bozana Radenkovic-Hadzic <bozana@raritan.com>
2015-10-25 22:21:26 -0400

    PUT and POST
    - handling PUT and POST at the root level
    - for POST I return what we get
    - for PUT I make some modifications on original data I get and then
      return it back

--- server_sim/README.md
+++ server_sim/README.md
@@ -15,7 +15,7 @@ Use this to simulate server response back to the client
 - open broswer and point it to: localhost:7001
   You should see HelloWorld jason's msg
 
-### Exapand it 
+### Expand it 
 
 #### GET call
  - as an example see hello.js
@@ -23,5 +23,15 @@ Use this to simulate server response back to the client
     - add new file, following the structure
     - add require in the app.js 
     - bind that to new path you will provide
+ - test from curl: 
+    - _curl -H Content-Type:application/json -H Accept:application/json  -X GET  http://localhost:7001/_
+
+### POST call
+ - test from curl:
+    - _curl -d '[{"a":"1"}, {"b":"2"}, {"c":"3"}]' -H "Content-Type: application/json" -X POST http://127.0.0.1:7001/_
+
+### PUT call
+ - test from curl:
+    -_curl -d '[{"a":"1"}, {"b":"2"}, {"c":"3"}]' -H "Content-Type: application/json" -X PUT http://127.0.0.1:7001/_ 
 
 
--- server_sim/app.js
+++ server_sim/app.js
@@ -1,12 +1,39 @@
 ﻿var express = require('express');
 var msg = require("./data1.js");
+var bodyParser = require('body-parser');
+var port = 7001;
 
 var app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+
+//For GET calls we are going to send back data we keep in data1
 app.get('/', function (req, res) {
-    //res.json({ message: 'hooray! welcome to our api!' });
+	console.log("GET /");
     res.json(msg.getData1());
 });
 
-//app.listen(process.env.PORT || 8080);
-app.listen(7001);
+
+//POST CALL, we sand back whatever we get
+app.post('/', function (req, res) {
+	console.log("POST /");
+	console.dir(req.body);
+	res.send(req.body);
+});
+
+//PUT call, get obj, do some modifications and send it back
+app.put('/', function (req, res) {
+	console.log("PUT /");
+	var obj = req.body;
+	console.dir(obj);
+	console.dir(obj[0].a);
+	obj[0].a = 'Hi';
+	res.send(obj);
+});
+
+
+console.log("Listening on port: " + port);
+app.listen(port);
+
+
