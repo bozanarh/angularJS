914dd549f74fc3f43a7d704b0fb2bf8a52b96a17
Bozana Radenkovic-Hadzic <bozana@raritan.com>
2015-10-27 08:39:09 -0400

    fixing CORS issue
    - When calling POST, chrom is ibehind hte scene sending OPTIONS call
    and only then after that it is sending POST.
    - Need to send AccessControl headers for OPTIONS also
    - TODO: Check if we have to send body for options

--- server_sim/app.js
+++ server_sim/app.js
@@ -1,5 +1,6 @@
 ﻿var express = require('express');
-var msg = require("./data1.js");
+var msg1 = require("./data1.js");
+var msg2 = require("./data2.js");
 var bodyParser = require('body-parser');
 var port = 7001;
 
@@ -11,7 +12,18 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 //For GET calls we are going to send back data we keep in data1
 app.get('/', function (req, res) {
 	console.log("GET /");
-    res.json(msg.getData1());
+    res.header('Access-Control-Allow-Headers' , 'Content-Type' );
+    res.header('Access-Control-Allow-Methods' , 'GET' );
+    res.header('Access-Control-Allow-Origin' , '*' );
+    res.json(msg1.getData1());
+});
+
+app.get('/api/v1/models', function (req, res) {
+	console.log("GET /api/v1/models/");
+    res.header('Access-Control-Allow-Headers' , 'Content-Type' );
+    res.header('Access-Control-Allow-Methods' , 'GET' );
+    res.header('Access-Control-Allow-Origin' , '*' );
+    res.json(msg2.getData());
 });
 
 
@@ -19,12 +31,38 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
 	console.log("POST /");
 	console.dir(req.body);
+    res.header('Access-Control-Allow-Headers' , 'Content-Type' );
+    res.header('Access-Control-Allow-Methods' , 'POST' );
+    res.header('Access-Control-Allow-Origin' , '*' );
+	res.send(req.body);
+});
+
+//POST CALL, we sand back whatever we get
+app.options('/api/v1/models', function (req, res) {
+	console.log("OPTIONS /api/v1/models");
+    res.header('Access-Control-Allow-Headers' , 'Content-Type' );
+    res.header('Access-Control-Allow-Methods' , 'POST, GET, OPTIONS, PUT' );
+    res.header('Access-Control-Allow-Origin' , '*' );
+	console.dir(req.body);
+	res.send(req.body);
+});
+
+//POST CALL, we sand back whatever we get
+app.post('/api/v1/models', function (req, res) {
+	console.log("POST /api/v1/models");
+    res.header('Access-Control-Allow-Headers' , 'Content-Type' );
+    res.header('Access-Control-Allow-Methods' , 'POST, GET, OPTIONS, PUT' );
+    res.header('Access-Control-Allow-Origin' , '*' );
+	console.dir(req.body);
 	res.send(req.body);
 });
 
 //PUT call, get obj, do some modifications and send it back
 app.put('/', function (req, res) {
 	console.log("PUT /");
+    res.header('Access-Control-Allow-Headers' , 'Content-Type' );
+    res.header('Access-Control-Allow-Methods' , 'PUT' );
+    res.header('Access-Control-Allow-Origin' , '*' );
 	var obj = req.body;
 	console.dir(obj);
 	console.dir(obj[0].a);
