7327748d608bbce31acdfe717d797ed2fd08b704
Bozana Radenkovic-Hadzic <bozana@raritan.com>
2015-10-27 10:55:03 -0400

    data for grid_with_editing
    - this data can be use also with soem other tests, like
      grid_with_filtering

---
+++ server_sim/data2.js
@@ -0,0 +1,23 @@
+(function() {
+    var data= [
+    {
+        "name": "Ethel Price",
+        "gender": "female",
+        "company": "Enersol"
+    },
+    {
+        "name": "Claudine Neal",
+        "gender": "female",
+        "company": "Sealoud"
+    },
+    {
+        "name": "Beryl Rice",
+        "gender": "female",
+        "company": "Velity"
+    }];
+
+    module.exports.getData = function() {
+        return data;
+    }
+}());
+
