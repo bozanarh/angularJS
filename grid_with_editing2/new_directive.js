e6664761b9be58c2169e3a8c90045fde5b954826
Bozana Radenkovic-Hadzic <bozana@raritan.com>
2015-11-02 14:20:54 -0500

    another directive example
    -w eare using directlye by the htmlheader here

---
+++ grid_with_editing2/new_directive.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function (angular){
+  angular
+    .module('app')
+
+    .controller('MyNewCtrl', ['$scope', function($scope) {
+      $scope.abc = 'Hi';
+    }])
+    .directive("selectAllCheckbox", function() {
+    return {
+        replace:true,
+        restrict: 'AE',
+        templateUrl: 'headerCellTemplate.html'
+        controller: 'app.myNewCtrl'
+    };
+
+})(angular));
+ 
